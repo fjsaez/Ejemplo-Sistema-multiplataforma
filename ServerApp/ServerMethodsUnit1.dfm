268f7e5cdcb468078d09f5b1624601f287a7d84c
Francisco Sáez <fjsaez@gmail.com>
2021-09-17 20:37:54 -0400

    Commit inicial

---
+++ ServerApp/ServerMethodsUnit1.dfm
@@ -0,0 +1,50 @@
+object ServerMethods1: TServerMethods1
+  OldCreateOrder = False
+  Height = 283
+  Width = 382
+  object FDConnection1: TFDConnection
+    Params.Strings = (
+      'Protocol=TCPIP'
+      'Server=192.168.0.10'
+      'User_Name=SYSDBA'
+      'Password=nasa030904'
+      'Database=D:\Datos\AZTLAN.FDB'
+      'DriverID=FB')
+    Connected = True
+    LoginPrompt = False
+    Left = 56
+    Top = 40
+  end
+  object FDPhysFBDriverLink1: TFDPhysFBDriverLink
+    Left = 176
+    Top = 40
+  end
+  object FDGUIxWaitCursor1: TFDGUIxWaitCursor
+    Provider = 'Forms'
+    Left = 288
+    Top = 40
+  end
+  object FDQ_Login: TFDQuery
+    Connection = FDConnection1
+    SQL.Strings = (
+      'select * '
+      'from Empleados e'
+      'where e.Usuario = :usr and e.Clave = :cve'
+      'rows 1')
+    Left = 56
+    Top = 112
+    ParamData = <
+      item
+        Name = 'USR'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 25
+      end
+      item
+        Name = 'CVE'
+        DataType = ftString
+        ParamType = ptInput
+        Size = 100
+      end>
+  end
+end
---
+++ ServerApp/ServerMethodsUnit1.pas
@@ -0,0 +1,59 @@
+﻿unit ServerMethodsUnit1;
+
+interface
+
+uses System.SysUtils, System.Classes, System.Json,
+    Datasnap.DSServer, Datasnap.DSAuth, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt,
+  FireDAC.Comp.DataSet, FireDAC.Comp.UI, FireDAC.Phys.IBBase;
+
+type
+{$METHODINFO ON}
+  TServerMethods1 = class(TDataModule)
+    FDConnection1: TFDConnection;
+    FDPhysFBDriverLink1: TFDPhysFBDriverLink;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    FDQ_Login: TFDQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    function EchoString(Value: string): string;
+    function ReverseString(Value: string): string;
+    function ValidaUsuario(const Usuario,Clave: string): boolean;
+  end;
+{$METHODINFO OFF}
+
+implementation
+
+{$R *.dfm}
+
+uses System.StrUtils;
+
+function TServerMethods1.EchoString(Value: string): string;
+begin
+  Result := Value;
+end;
+
+function TServerMethods1.ReverseString(Value: string): string;
+begin
+  Result := System.StrUtils.ReverseString(Value);
+end;
+
+function TServerMethods1.ValidaUsuario(const Usuario, Clave: string): boolean;
+begin
+  Result:=false;
+  FDQ_Login.Active:=false;
+  FDQ_Login.Params[0].Value:=Usuario;
+  FDQ_Login.Params[1].Value:=Clave;
+  FDQ_Login.Prepare;
+  FDQ_Login.Open;
+  Result:=not FDQ_Login.IsEmpty;
+  {if not FDQ_Login.IsEmpty then
+    Result:=true;}
+end;
+
+end.
